ce553ccf90e157d213fed7fe4866d53cbde698f1
Votre Nom <philippe.michel@ght-novo.fr>
2026-01-08 15:07:20 +0100

    Validation des lecteurs OK V1.1

--- import_validation.R
+++ import_validation.R
@@ -21,33 +21,45 @@ import_val <- function(sh) {
     "", " ", "NA", "ND", "#DIV/0", "!", "NC", "Non visible", "NON MESURABLE", "non evaluable",
     "PAS DIMAGES SANS MESURES", "IMPOSSIBLE", "non évaluable", "#DIV/0 !"
   )
-  nom <- read_ods("datas/validation.ods", sheet = sh, range = "C4:C5", col_names = FALSE)
-  print(nom)
+  nfg <- c("nf_dt", "gn_dt", "nf_gch", "gn_gch")
+  nomi <- read_ods("datas/validation.ods", sheet = sh, range = "C4:C5", col_names = FALSE)
+  print(nomi)
   lect <- read_ods("datas/validation.ods", sheet = sh, range = "B11:E23", col_names = FALSE, na = nna) |>
     dplyr::filter(...1 != "zz" | is.na(...1)) |>
     mutate_all(as.numeric)
+  names(lect) <- nfg
+  lecteur <- lect |>
+    pivot_longer(cols = everything(), values_to = "lecteur")
   exp <- read_ods("datas/validation.ods", sheet = sh, range = "R11:U23", col_names = FALSE, na = nna) |>
     dplyr::filter(...1 != "zz" | is.na(...1)) |>
     mutate_all(as.numeric)
-  dif <- abs(exp - lect)
-  names(lect) <- paste0("lect", 1:4)
-  names(dif)[1:4] <- c("nf_dt", "gn_dt", "nf_gch", "gn_gch")
+  names(exp) <- nfg
+  expert <- exp |>
+    pivot_longer(cols = everything(), values_to = "expert")
+  nom <- rep(paste0(nomi[1, 1], " (C", nomi[2, 1], ")"), nrow(expert))
   #
-  nf <- c(dif$nf_dt, dif$nf_gch)
-  gn <- c(dif$gn_dt, dif$gn_gch)
-  nom <- rep(paste0(nom[1, 1], " (c", nom[2, 1], ")"), length(nf))
-  ll <- tibble(nom = nom, nerf = nf, gaine = gn)
+  tt <- lecteur |>
+    mutate(expert = expert$expert, nom = nom) |>
+    mutate(dif = abs(expert - lecteur)) |>
+    mutate(type = fct_recode(name,
+      "gaine" = "gn_dt",
+      "gaine" = "gn_gch",
+      "nerf" = "nf_dt",
+      "nerf" = "nf_gch"
+    )) |>
+    mutate_if(is.character, as.factor)
+  #
+  return(tt)
 }
 #
-val <- NULL
-for (sh in 1:nsh) {
-  ll <- import_val(sh)
-  val <- bind_rows(val, ll)
-}
-#
-var_label(val$nom) <- "Lecteur"
-var_label(val$nerf) <- "Nerf optique (mm)"
-var_label(val$gaine) <- "Gaine du nerf optique (mm)"
 
-save(val, file = "datas/validation.RData")
+tt <- NULL
+for (sh in 1:nsh) {
+  zz <- import_val(sh)
+  tt <- rbind(tt, zz)
+}
+#
+
+
+save(tt, file = "datas/validation.RData")
 load("datas/validation.RData")
